67680a9bc69367edef9f4838c8a3ab79834a5684
TheConnMan <bcconn2112@gmail.com>
2015-12-12 08:44:31 -0500

    Pass query from the landing controller to the result controller

--- grails-app/assets/javascripts/home/index.js
+++ grails-app/assets/javascripts/home/index.js
@@ -14,10 +14,14 @@ app.config(['$routeProvider', function($routeProvider) {
 	});
 }]);
 
-app.controller('LandingCtrl', ['$scope', function($scope) {
+app.controller('LandingCtrl', ['$scope', '$location', function($scope, $location) {
 	$('#search').focus();
+
+	$scope.search = function() {
+		$location.path('/search/' + encodeURIComponent($scope.query));
+	};
 }]);
 
-app.controller('ResultsCtrl', ['$scope', function($scope) {
-
+app.controller('ResultsCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+	$scope.query = decodeURIComponent($routeParams.query);
 }]);

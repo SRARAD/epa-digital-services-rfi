251d0e40ddef52da8d928f07dd3fd15ce458624c
TheConnMan <bcconn2112@gmail.com>
2015-12-15 12:38:34 -0500

    Add hourly airnow data parsing

--- src/airnow.js
+++ src/airnow.js
@@ -11,14 +11,24 @@ try {
 
 var ftpHost = 'ftp.airnowapi.org';
 var locationsUri = '/Locations/monitoring_site_locations.dat';
+var hourlyUriRoot = '/HourlyData/';
 
 var locationArray = [];
+var hourlyDataObject = {};
 
 module.exports = {
 	init: function() {
-		pullFtpFile(locationsUri)
+		console.log('Pulling Locations Data');
+		pullFtpFile(hourlyUriRoot + constructHourlyFileName())
 		.then(function(targetLocation) {
 			locationArray = parseLocationFile(targetLocation);
+		})
+		.then(function() {
+			console.log('Pulling Hourly Data');
+			return pullFtpFile(locationsUri);
+		})
+		.then(function(targetLocation) {
+			hourlyDataObject = parseHourlyData(targetLocation);
 		})
 		.catch(function(error) {
 			console.log(error);
@@ -57,4 +67,30 @@ function parseLocationFile(targetLocation) {
 	}).filter(function(obj) {
 		return obj.id;
 	});
+}
+
+function parseHourlyData(targetLocation) {
+	var rawData = fs.readFileSync(targetLocation, 'utf-8');
+	return rawData.split('\n').reduce(function(all, row) {
+		var columns = row.split('|');
+		if (!all[columns[2]]) {
+			all[columns[2]] = {
+				date: columns[0] + ' ' + columns[1],
+				location: columns[3],
+				name: columns[8],
+				readings: []
+			};
+		}
+		all[columns[2]].readings.push({
+			label: columns[5],
+			units: columns[6],
+			value: columns[7]
+		});
+		return all;
+	}, {});
+}
+
+function constructHourlyFileName() {
+	var date = new Date();
+	return date.getFullYear().toString() + date.getUTCMonth().toString() + date.getUTCDate().toString() + date.getUTCHours().toString() + '.dat';
 }
